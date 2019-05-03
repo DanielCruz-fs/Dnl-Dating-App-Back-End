37a9482ffa3feb300b863460148d0395909155c3
unknown <dannyfsociety@gmail.com>
2019-05-03 10:44:57 -0400

    Initial Commit :runner:

---
+++ DatingAppBack/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingAppBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppBack.Data
+{   //we created this class which is the data access layer
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
