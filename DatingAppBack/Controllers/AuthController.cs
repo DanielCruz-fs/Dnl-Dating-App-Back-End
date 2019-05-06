f15115d5ad1a1686eb2f0fa802eaf1087eddbd11
unknown <dannyfsociety@gmail.com>
2019-05-06 16:24:15 -0400

    JWT response from server done

--- DatingAppBack/Controllers/AuthController.cs
+++ DatingAppBack/Controllers/AuthController.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingAppBack.Models;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingAppBack.Controllers
 {   
@@ -14,10 +19,12 @@ namespace DatingAppBack.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository repo;
+        private readonly IConfiguration config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             this.repo = repo;
+            this.config = config;
         }
 
         [HttpPost("register")]
@@ -32,6 +39,37 @@ namespace DatingAppBack.Controllers
             var createdUser = await this.repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await this.repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+            // JWT implementation
+            //this is the payload
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+            //we got the secret key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            //we build jwt
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //THE Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }
